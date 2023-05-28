f2ef65ececb68d2316ef0ec15620259a0505041e
captainreck00 <117888395+captainreck00@users.noreply.github.com>
2023-05-28 17:49:53 +0000

    Yes

---
+++ FUnc/P2.py
@@ -0,0 +1,4 @@
+def arithmetic(a,b):
+    return a+b,a-b,a*b,a/b
+
+print(arithmetic(4,7))
---
+++ FUnc/P3.py
@@ -0,0 +1,8 @@
+import random
+
+def rand (a,b):
+    r=random.randrange(a+1,b)
+    return r
+print(rand(1,34))
+print(rand(1,34))
+print(rand(1,34))
