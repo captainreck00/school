80067f372e02f1cc8b91e04365972e73e4d353c6
captainreck00 <117888395+captainreck00@users.noreply.github.com>
2023-08-25 04:11:33 +0000

    Do it

--- File handling/p10.py
+++ File handling/p10.py
@@ -1,10 +1,13 @@
 file=open("article.txt",'r+')
 l=file.readlines()
-file.close()
+print(l)
+
 vowel_l=["a","e","i","o","u"]
 vowel_u=["A","E","I","O","U"]
-n_str=""
+
+n=""
 for i in l:
+    n_str=""
     for j in i:
         if j in vowel_l:
             n_str+=j.upper()
@@ -12,6 +15,14 @@ for i in l:
             n_str+=j.lower()
         else:
             n_str+=j
+    n+=n_str       
 
-print(n_str)            
+file.seek(0)
+file.write(n)
+           
+file.close()
 
+
+# dsf asdf ato  OOOOfasdf to sdaf ads the adsf aSSDsdf
+# asdfWEWQRPASikj to the tABDSXo the adsaf the to tothe asdfWEWQRPASikj
+# DAOUe
--- File handling/p11.py
+++ File handling/p11.py
@@ -1,16 +1,16 @@
-myfile = open("marks.dat","a+")
-stnum = int(input("Enter number of students: "))
-for i in range(stnum):
-    Name = input("Enter name: ")
-    M = int(input("Enter marks in math: "))
-    E = int(input("Enter marks in english: "))
-    CS = int(input("Enter marks in cs: "))
-    P = int(input("Enter marks in physics: "))
-    C = int(input("Enter marks in chemistry: "))
-    TM =M+E+CS+P+C
-    Per = (TM/5)
-    l = ["Name:",Name, "\n", "Math:",str(M), "\n", "Eng:",str(E),"\n","CS:",str(CS),"\n","Phy:",str(P),"\n","Chem:",str(C),"\n","Total Marks:",str(TM),"\n","Per:",str(Per), "\n"]
-    myfile.writelines(l)
-myfile.seek(0)
-str0 = myfile.readlines()
-print(str0)
+import pickle
+
+name=input("Enter your name")
+p=int(input("Enter your marks in physics"))
+m=int(input("Enter your marks in maths"))
+c=int(input("Enter your marks in chemistry"))
+cs=int(input("Enter your marks in CS"))
+e=int(input("Enter your marks in english"))
+per=(p+e+m+c+cs)/5
+d={"name":name,"physics":p,"maths":m,"chemistry":c,"computer":cs}
+
+file=open("marks.dat","ab")
+
+pickle.dump(d,file)
+
+print(pickle.load(file))
